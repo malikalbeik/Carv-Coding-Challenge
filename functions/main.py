9dfb6722fccc5229d0457e7bd5bba12154d89c39
Malik Albeik <albeik.malik@gmail.com>
2024-10-14 17:37:25 +0300

    feat: changed function region

--- functions/main.py
+++ functions/main.py
@@ -8,8 +8,6 @@ from firebase_admin import initialize_app
 initialize_app()
 
 
-@https_fn.on_request()
-def on_request_example(
-    req: https_fn.Request, region="europe-west4"
-) -> https_fn.Response:
+@https_fn.on_request(region="europe-west4")
+def on_request_example(req: https_fn.Request) -> https_fn.Response:
     return https_fn.Response("Hello world!")
